3e82c7fed4de1fde0a51c3bc66075d46841f5462
Nick Girardo <nickgirardo@gmail.com>
2021-09-11 20:01:20 -0400

    Display json contents for debugging

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { useState } from 'react';
+
 import { FileInput } from './components/FileInput';
 
-export default () => (
-  <FileInput
-    onData={ data => console.log(data) }
-    onError={ err => console.error(err) }
-  />
-);
+export default () => {
+  const [bundle, setBundle] = useState<any | null>(null);
+
+  return (
+    <>
+      <FileInput
+        onData={ data => setBundle(data) }
+        onError={ err => console.error(err) }
+      />
+      { bundle &&
+        <pre>{ JSON.stringify(bundle, null, 2) }</pre>
+      }
+    </>
+  );
+};
